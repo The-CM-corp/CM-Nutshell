95dd68d390576ef7d4d40053844792958c1430cc
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-09 14:01:32 -0600

    fixed merge conflicts

--- src/scripts/DOMPopulator.js
+++ src/scripts/DOMPopulator.js
@@ -2,6 +2,24 @@ import elementCreator from "./elementFactory"
 // generic template
 
 const putOnDOM = {
+  postNewMessage(chatObject) {
+    let name = elementCreator.elementFactory("a", "placeholder for username", null, null, "")
+    let time = elementCreator.elementFactory("p", chatObject.time, null, null, null, null)
+    let message = elementCreator.elementFactory("p", chatObject.message, null, null, null, null)
+    let messageHolder = elementCreator.elementFactory("section", null, "chatSection", `message-${chat.id}`, name, time, message)
+    //appending our new elements to the fragment then the fragment to or article
+    let fragment = document.createDocumentFragment()
+    let chatOutput = document.querySelector("#chat__results")
+    fragment.appendChild(messageHolder)
+    chatOutput.appendChild(fragment)
+  },
+
+  // loops over all of one type of entry and populates multiple ones at a time
+  initialChat(chatObjects) {
+    chatObjects.forEach(chatObject => {
+      putOnDOM.postNewMessage(chatObject)
+    })
+  },
   postNewToDO(entry) {
     let entryTask = elementCreator.elementFactory("p", entry.task, "toDo__task", `ToDoTask__${entry.id}`)
     let entryDate = elementCreator.elementFactory("p", entry.date, "toDO__date", `ToDoDate__${entry.id}`)
