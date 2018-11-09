8b5c63b596cf5b2d6e89370cfc1fb85238500c5d
BryanNilsen <bryannilsen@gmail.com>
2018-11-09 14:54:48 -0600

    index boilerplate, main.css starter file, refactored dompopulator

--- src/scripts/DOMPopulator.js
+++ src/scripts/DOMPopulator.js
@@ -2,45 +2,45 @@ import elementCreator from "./elementFactory"
 // generic template
 
 const putOnDOM = {
-  postNewMessage(chatObject) {
-    let name = elementCreator.elementFactory("a", "placeholder for username", null, null, "")
-    let time = elementCreator.elementFactory("p", chatObject.time, null, null, null, null)
-    let message = elementCreator.elementFactory("p", chatObject.message, null, null, null, null)
-    let messageHolder = elementCreator.elementFactory("section", null, "chatSection", `message-${chat.id}`, name, time, message)
+  postNewMessage(entry) {
+    let chatName = elementCreator.elementFactory("a", entry.user_id, "chat__name", `chat__name${entry.id}`, "#")
+    let chatTime = elementCreator.elementFactory("p", entry.time, "chat__time", `chat__time${entry.id}`)
+    let chatMessage = elementCreator.elementFactory("p", entry.message, "chat__message", `chat_message${entry.id}`)
+    let chatHolder = elementCreator.elementFactory("div", null, "chat__div", `chat__div${entry.id}`, null, chatName, chatTime, chatMessage)
     //appending our new elements to the fragment then the fragment to or article
     let fragment = document.createDocumentFragment()
     let chatOutput = document.querySelector("#chat__results")
-    fragment.appendChild(messageHolder)
+    fragment.appendChild(chatHolder)
     chatOutput.appendChild(fragment)
   },
 
   // loops over all of one type of entry and populates multiple ones at a time
-  initialChat(chatObjects) {
-    chatObjects.forEach(chatObject => {
-      putOnDOM.postNewMessage(chatObject)
+  initialChat(entries) {
+    entries.forEach(entry => {
+      putOnDOM.postNewMessage(entry)
     })
   },
-  postNewToDO(entry) {
-    let entryTask = elementCreator.elementFactory("p", entry.task, "toDo__task", `ToDoTask__${entry.id}`)
-    let entryDate = elementCreator.elementFactory("p", entry.date, "toDO__date", `ToDoDate__${entry.id}`)
-    let entryCheck = elementCreator.elementFactory("input type='checkbox'", null, "toDO__checkbox", `ToDoCheckbox__${entry.id}`)
-    let entryHolder = elementCreator.elementFactory("div", null, "toDoDiv", `ToDo__${entry.id}`, entryTask, entryDate, entryCheck)
+  postNewTodo(entry) {
+    let entryTask = elementCreator.elementFactory("p", entry.task, "todo__task", `todo__task${entry.id}`)
+    let entryDate = elementCreator.elementFactory("p", entry.date, "todo__date", `todo__date${entry.id}`)
+    let entryCheck = elementCreator.elementFactory("input type='checkbox'", null, "todo__checkbox", `todo__checkbox${entry.id}`)
+    let entryHolder = elementCreator.elementFactory("div", null, "todo__div", `todo__div${entry.id}`, null, entryTask, entryDate, entryCheck)
     //appending our new elements to the fragment then the fragment to or article
     let fragment = document.createDocumentFragment()
     fragment.appendChild(entryHolder)
-    let toDoOutput = document.getElementById("todo__results")
-    toDoOutput.appendChild(fragment)
+    let todoOutput = document.getElementById("todo__results")
+    todoOutput.appendChild(fragment)
   },
-  initialToDos(entries) {
+  initialTodos(entries) {
     entries.forEach(entry => {
-      putOnDOM.postNewToDO(entry)
+      putOnDOM.postNewTodo(entry)
     })
   },
   postNewNews(entry) {
     let newsTitle = elementCreator.elementFactory("h2", entry.title, "news__title", `news__title${entry.id}`)
     let newsSynopsis = elementCreator.elementFactory("p", entry.synopsis, "news__synopsis", `news__synopsis${entry.id}`)
     let newsURL = elementCreator.elementFactory("p", entry.url, "news__url", `news__url${entry.id}`)
-    let newsHolder = elementCreator.elementFactory("div", null, "news__section", `news__section${news.id}`, newsTitle, newsSynopsis, newsURL)
+    let newsHolder = elementCreator.elementFactory("div", null, "news__div", `news__div${entry.id}`, null, newsTitle, newsSynopsis, newsURL)
     //appending our new elements to the fragment then the fragment to or article
     let fragment = document.createDocumentFragment()
     fragment.appendChild(newsHolder)
@@ -53,6 +53,26 @@ const putOnDOM = {
   intialNews(entries) {
     entries.forEach(entry => {
       putOnDOM.postNewNews(entry)
+    })
+  },
+  postNewEvent(entry) {
+    let eventTitle = elementCreator.elementFactory("h2", entry.title, "event__title", `event__title${entry.id}`)
+    let eventSynopsis = elementCreator.elementFactory("p", entry.synopsis, "event__synopsis", `event__synopsis${entry.id}`)
+    let eventDate = elementCreator.elementFactory("p", entry.date, "event__date", `event__date${entry.id}`)
+    let eventLocation = elementCreator.elementFactory("p", entry.location, "event__location", `event__location${entry.id}`)
+    let eventHolder = elementCreator.elementFactory("div", null, "event__div", `event__div${event.id}`, null, eventTitle, eventDate, eventSynopsis, eventLocation)
+    //appending our new elements to the fragment then the fragment to or article
+    let fragment = document.createDocumentFragment()
+    fragment.appendChild(eventHolder)
+
+    let eventOutput = document.getElementById("event__results")
+    eventOutput.appendChild(fragment)
+  },
+
+  // loops over all of one type of entry and populates multiple ones at a time
+  intialEvents(entries) {
+    entries.forEach(entry => {
+      putOnDOM.postNewEvent(entry)
     })
   }
 }
