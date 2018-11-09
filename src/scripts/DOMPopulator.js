1277ba73e701b19d2c46f870dfa15ee2845fedce
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-09 13:36:22 -0600

    added element factory for the news data

--- src/scripts/DOMPopulator.js
+++ src/scripts/DOMPopulator.js
@@ -1,20 +1,41 @@
 import elementCreator from "./elementFactory"
 // ?generic template
 const putOnDOM = {
-  postNewEntry(entry) {
-    let entryTitle = elementCreator.elementFactory("h2", entry.concept)
-    let entryContent = elementCreator.elementFactory("p", `${entry.entry} I am ${entry.mood}. ${entry.date}`)
-    let entryHolder = elementCreator.elementFactory("section", null, "entrySection", `${entry.id}`, entryTitle, entryContent)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  postNewNews(entry) {
+    let newsTitle = elementCreator.elementFactory("h2", entry.title, "news__title", `news__title${entry.id}`)
+    let newsSynopsis = elementCreator.elementFactory("p", entry.synopsis, "news__synopsis", `news__synopsis${entry.id}`)
+    let newsURL = elementCreator.elementFactory("p", entry.url, "news__url", `news__url${entry.id}`)
+    let newsHolder = elementCreator.elementFactory("div", null, "news__section", `news__section${news.id}`, newsTitle, newsSynopsis, newsURL)
     //appending our new elements to the fragment then the fragment to or article  
     let fragment = document.createDocumentFragment()
-    fragment.appendChild(entryHolder)
-    entryArt.appendChild(fragment)
+    fragment.appendChild(newsHolder)
+
+    let newsOutput = document.getElementById("news__results")
+    newsOutput.appendChild(fragment)
   },
 
   // loops over all of one type of entry and populates multiple ones at a time
-  domCreation(entries) {
+  intialNews(entries) {
     entries.forEach(entry => {
-      putOnDOM.postNewEntry(entry)
+      putOnDOM.postNewNews(entry)
 
     })
   }
