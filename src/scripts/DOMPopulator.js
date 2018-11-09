1fa8bb639bb721d71f441458700f000dcd160d0b
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-09 13:49:54 -0600

    added methods on the DOM populator object to put chat messages on the DOM

--- src/scripts/DOMPopulator.js
+++ src/scripts/DOMPopulator.js
@@ -1,21 +1,22 @@
 import elementCreator from "./elementFactory"
 // ?generic template
 const putOnDOM = {
-  postNewEntry(entry) {
-    let entryTitle = elementCreator.elementFactory("h2", entry.concept)
-    let entryContent = elementCreator.elementFactory("p", `${entry.entry} I am ${entry.mood}. ${entry.date}`)
-    let entryHolder = elementCreator.elementFactory("section", null, "entrySection", `${entry.id}`, entryTitle, entryContent)
-    //appending our new elements to the fragment then the fragment to or article  
+  postNewMessage(chatObject) {
+    let name = elementCreator.elementFactory("a", "placeholder for username", null, null, "")
+    let time = elementCreator.elementFactory("p", chatObject.time, null, null, null, null)
+    let message = elementCreator.elementFactory("p", chatObject.message, null, null, null, null)
+    let messageHolder = elementCreator.elementFactory("section", null, "chatSection", `message-${chat.id}`, name, time, message)
+    //appending our new elements to the fragment then the fragment to or article
     let fragment = document.createDocumentFragment()
-    fragment.appendChild(entryHolder)
-    entryArt.appendChild(fragment)
+    let chatOutput = document.querySelector("#chat__results")
+    fragment.appendChild(messageHolder)
+    chatOutput.appendChild(fragment)
   },
 
   // loops over all of one type of entry and populates multiple ones at a time
-  domCreation(entries) {
-    entries.forEach(entry => {
-      putOnDOM.postNewEntry(entry)
-
+  initialChat(chatObjects) {
+    chatObjects.forEach(chatObject => {
+      putOnDOM.postNewMessage(chatObject)
     })
   }
 }
--- src/scripts/elementFactory.js
+++ src/scripts/elementFactory.js
@@ -1,10 +1,11 @@
 // function to create any elements (el) with any content (cont or null) and you can designate if it has a child
 const elementCreator = {
-  elementFactory(el, cont, clazz, id, ...children) {
+  elementFactory(el, cont, clazz, id, link, ...children) {
     let element = document.createElement(el)
     $(element).html(cont || null)
     $(element).addClass(clazz || null)
     element.setAttribute("id", id || null)
+    element.setAttribute("href", link || null)
     children.forEach(child => {
       element.appendChild(child)
     })
