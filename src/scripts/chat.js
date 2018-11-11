4b2c276e202d03bba80d8e7d9ba8b20a9cd3e82b
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-11-10 20:12:20 -0600

    got fetch calls working to add new chats to the DB, edit a chat, delete a chat, and print all chats to the console. All on page load

---
+++ src/scripts/chat.js
@@ -0,0 +1,37 @@
+// communicate with the API to GET the messages array of Objects, then be able to obtain the value name on the user id. Be able to edit (PATCH) one entry, POST a new entry, and DELTE an entry. Start by getting these to function without dealing with the DOM yet.
+
+const apiChat = {
+  saveNewChat: function (object) {
+    return fetch(`http://localhost:8088/chats/`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(object)
+    })
+  },
+  editChat: function (objId, newContent) {
+    return fetch(`http://localhost:8088/chats/${objId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newContent)
+    })
+  },
+  deleteChat: function (objectId) {
+    return fetch(`http://localhost:8088/chats/${objectId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  },
+  loadExistingChats: function () {
+    return fetch(`http://localhost:8088/chats/`)
+    .then(entries => entries.json())
+    .then(entries => entries)
+  }
+}
+
+export default apiChat
