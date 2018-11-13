f47b6d591a40b05a4474a2f863b297ba650784a0
amazoradi <amazoradi@gmail.com>
2018-11-12 19:21:13 -0600

    checked todos move to the checked section

--- src/scripts/todoDelete.js
+++ src/scripts/todoDelete.js
@@ -5,7 +5,8 @@ function clearTODODom() {
   document.getElementById("todo__results").innerHTML = ""
   API.getTodo()
     .then(todo => {
-      putOnDOM.initialTodos(todo)})
+      putOnDOM.initialTodos(todo)
+    })
 }
 
 const todoClicks = () => {
@@ -26,18 +27,41 @@ const todoClicks = () => {
       // })
     }
   })
+  // try adding event listener to whole big div around results and checked divs
   document.getElementById("todo__results").addEventListener("change", event => {
-    if (event.target.id.startsWith("todoCheckbox")){
+    if (event.target.id.startsWith("todoCheckbox")) {
       const id = event.target.id.split("!")[1]
       console.log("checked id", id)
       let text = document.getElementById(`todo__text!${id}`)
-      console.log(text) 
-      text.classList.toggle("todo__checked")
-      // if (id === text.id.split("!")[1]){
-      //   console.log(text)
-      // }
+      console.log(text)
+      let checkTaskDiv = document.getElementById(`todo__div${id}`)
+      let checkedDiv = document.getElementById("todo__complete")
+      if (event.target.checked === true) {
+        console.log("Checked is true")
+        text.classList.add("todo__checked")
+        checkedDiv.appendChild(checkTaskDiv)
+      }
     }
   })
+  // this may work but change the id here 
+  document.getElementById("todo__complete").addEventListener("change", event => {
+    let uncheckedDiv = document.getElementById("todo__results")
+    // let checkedDiv = document.getElementById("todo__complete")
+    if (event.target.checked === false) {
+      const id = event.target.id.split("!")[1]
+      console.log("checked id", id)
+      let text = document.getElementById(`todo__text!${id}`)
+      let checkTaskDiv = document.getElementById(`todo__div${id}`)
+      console.log("unchecked is false")
+      text.classList.remove("todo__checked")
+      uncheckedDiv.appendChild(checkTaskDiv)
+    }
+  })
+
+  // if (id === text.id.split("!")[1]){
+  //   console.log(text)
+  // }
+
 }
 
 
